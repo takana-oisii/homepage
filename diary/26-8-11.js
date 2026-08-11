d7fd6e10fe1137c1cd3708a9adb92d047fa2a4de
takuann2438-dotcom <takuann2438@gmail.com>
2026-08-11 19:17:49 +0900

    RNG記事にトグルスイッチを追加し、表示モードを切り替えられるようにしました

--- diary/26-8-11.js
+++ diary/26-8-11.js
@@ -1,11 +1,20 @@
 const button = document.getElementById('roll')
 const output = document.getElementById('kekka')
+const mode = document.getElementById('mode')
 
 
 let kekka
 
 button.addEventListener('click', () => {
-    kekka = Math.floor(Math.random()*10000000)
-    kekka = `You rolled... 1 in 10000000 ${kekka}`
+    console.log(mode.checked)
+    if (mode.checked) {
+        kekka = Math.floor(Math.random() * 1000000000)
+        kekka = kekka.toString(36)
+        kekka = `You rolled... 1 in 1000000000 ${kekka}`
+    } 
+    else {
+        kekka = Math.floor(Math.random() * 10000000)
+        kekka = `You rolled... 1 in 10000000 ${kekka}`
+    }
     output.textContent = kekka
 });
